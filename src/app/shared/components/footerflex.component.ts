569790dc81b0bb93b3db6679d22a3e1192bc0373
Pawel Tomczyk <pawel.tomczyk@outlook.com>
2020-01-03 14:52:10 +0100

    EOD_20200103

--- src/app/shared/components/footerflex.component.ts
+++ src/app/shared/components/footerflex.component.ts
@@ -11,53 +11,33 @@ export class FooterFlexComponent {
     @Input() achievementsButtonVisible: boolean;
     @Input() workplanButtonVisible: boolean;
 
-    private buttonWorkplanCaption: string;
-    private buttonAffirmationsCaption: string;
-    private buttonAchievementsCaption: string;
-    private isHomeCaptionDisplayed: boolean;
-    private captionReplacedByHome: string;
+    private buttonWorkplanCaption: string = 'Workplan';
+    private buttonAffirmationsCaption: string = 'Affirmations';
+    private buttonAchievementsCaption: string = 'Achievements';
+    private isHomeCaptionDisplayed: boolean = false;
+    private captionReplacedByHome: string = '';
 
     constructor(private rtr: Router){
         this.affirmationsButtonVisible = true;
         this.achievementsButtonVisible = true;
         this.workplanButtonVisible = true;
-
-        this.buttonAchievementsCaption = 'Achievements';
-        this.buttonAffirmationsCaption = 'Affirmations';
-        this.buttonWorkplanCaption = 'Workplan';
-        this.isHomeCaptionDisplayed = false;
-        this.captionReplacedByHome = '';
-    }
-
-    setHomeCaptionOnButton(dir: string){
-        switch(dir){
-            case '/workplan':
-                {
-                    this.captionReplacedByHome != '' ? this.buttonWorkplanCaption = 'Home' : this.buttonAchievementsCaption = 'Workplan';
-                    this.captionReplacedByHome = dir;
-                    break;
-                }
-            case '/affirmations':
-                {
-                    this.captionReplacedByHome != '' ? this.buttonAffirmationsCaption = 'Home' : this.buttonAffirmationsCaption = 'Affirmations';
-                    this.captionReplacedByHome = dir;
-                    break;
-                }
-            case '/achievements':
-                {
-                    this.captionReplacedByHome != '' ? this.buttonAchievementsCaption = 'Home' : this.buttonAchievementsCaption = 'Achievements';
-                    this.captionReplacedByHome = dir;
-                    break;
-                }
-        }
     }
 
     navigateTo(direction: string) {
         switch(direction) {
             case '/workplan':
                 {
-                    this.captionReplacedByHome == '/workplan' ? this.rtr.navigateByUrl('') : this.rtr.navigateByUrl('/workplan');
-                    this.setHomeCaptionOnButton(direction);
+                    console.log('CaptionReplacedByHome: ', this.captionReplacedByHome, ' , direction: ', direction);
+
+                    if(this.captionReplacedByHome != '') {
+                        this.buttonWorkplanCaption = 'Workplan';
+                        this.captionReplacedByHome = '';
+                        this.rtr.navigateByUrl('/home');
+                    } else {
+                        this.buttonWorkplanCaption = 'Home';
+                        this.captionReplacedByHome = 'Workplan';
+                        this.rtr.navigateByUrl('/workplan');
+                    }
                     break;
                 }
         }
